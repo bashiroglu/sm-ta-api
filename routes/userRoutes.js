9189c88ecfaef25a43773cb8dc304e5469231551
Elvin Bashirli <ebashirli@gmail.com>
2023-09-25 15:29:28 +0400

    remove update me and delete me functionalities

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,33 +1,24 @@
 const express = require("express");
 const {
   getUser,
-  getOneByRole,
   getUsers,
   getAllByRole,
   createUser,
   updateUser,
   deleteUser,
   getMe,
-  updateMe,
-  resizeUserPhoto,
-  uploadUserPhoto,
-  deleteMe,
 } = require("./../controllers/userController");
 const {
   protect,
   updatePassword,
   restrictTo,
 } = require("../controllers/authController");
-const { getAll } = require("../controllers/handlerFactory");
 
 const router = express.Router();
 
 // Protect all routes after this middleware
 router.use(protect);
-router.patch("/updateMyPassword", updatePassword);
 router.get("/me", getMe, getUser);
-router.patch("/updateMe", uploadUserPhoto, resizeUserPhoto, updateMe);
-router.delete("/deleteMe", deleteMe);
 
 router.use(restrictTo("admin"));
 router.route("/").get(getUsers).post(createUser);
