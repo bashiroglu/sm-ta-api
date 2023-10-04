2bd88d6dff172a4c4eea975843d1c21bd4a5ca10
Elvin Bashirli <ebashirli@gmail.com>
2023-10-04 19:51:46 +0400

    fix enums

--- utils/constants/enums.js
+++ utils/constants/enums.js
@@ -1,13 +1,15 @@
-exports.employeeRoles = Object.freeze({
+const employeeRoles = Object.freeze({
   OWNER: "owner",
   EMPLOYEE: "employee",
   MANAGER: "manager",
   TEACHER: "teacher",
 });
 
-exports.roles = Object.freeze({
-  ...module.employeeRoles,
+const roles = Object.freeze({
+  ...employeeRoles,
   ADMIN: "admin",
   STUDENT: "student",
   GUARDIAN: "guardian",
 });
+
+module.exports = { roles, employeeRoles };
