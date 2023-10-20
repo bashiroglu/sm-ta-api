0c2a262cf7e502e8278f47b691ca5b206f5aec2a
Elvin Bashirli <ebashirli@gmail.com>
2023-10-20 17:44:35 +0400

    populate branch.name in get room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,5 +1,6 @@
 const factory = require("./helpers/handlerFactory");
 const RoomModel = require("../models/roomModel");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getRooms = factory.getAll(RoomModel);
 exports.getRoom = factory.getOne(RoomModel);
@@ -7,3 +8,8 @@ exports.createRoom = factory.createOne(RoomModel);
 exports.updateRoom = factory.updateOne(RoomModel);
 exports.archiveRoom = factory.archiveOne(RoomModel);
 exports.deleteRoom = factory.deleteOne(RoomModel);
+
+exports.populate = catchAsync(async (req, res, next) => {
+  req.popOptions = { path: "branch" };
+  next();
+});
