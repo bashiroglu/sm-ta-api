40c96ea0bbb90010cc695e7b2a22b8091b9844e2
Elvin Bashirli <ebashirli@gmail.com>
2023-10-22 19:35:30 +0400

    add sorty by priority

--- controllers/lowerCategoryController.js
+++ controllers/lowerCategoryController.js
@@ -13,3 +13,8 @@ exports.queryByUpper = catchAsync(async (req, res, next) => {
   req.query = { ...req.query, upperCategory: req.params.upperId };
   next();
 });
+
+exports.sortDescending = catchAsync(async (req, res, next) => {
+  req.query.sort = "-priority";
+  next();
+});
--- routes/lower-categories.js
+++ routes/lower-categories.js
@@ -7,19 +7,25 @@ const {
   archiveLowerCategory,
   deleteLowerCategory,
   queryByUpper,
+  sortDescending,
 } = require("../controllers/lowerCategoryController");
 const { protect, restrictTo } = require("../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
 router.use(protect, restrictTo("roles", "owner", "admin", "manager"));
-router.route("/").get(getLowerCategories).post(createLowerCategory);
+router
+  .route("/")
+  .get(sortDescending, getLowerCategories)
+  .post(createLowerCategory);
 router
   .route("/:id")
   .get(getLowerCategory)
   .patch(updateLowerCategory)
   .delete(deleteLowerCategory);
 router.route("/:id/archive").patch(archiveLowerCategory);
-router.route("/upper/:upperId").get(queryByUpper, getLowerCategories);
+router
+  .route("/upper/:upperId")
+  .get(queryByUpper, sortDescending, getLowerCategories);
 
 module.exports = router;
