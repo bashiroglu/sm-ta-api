47ccbb2ef490ad140741d56ea04b5dab1bdd783d
Elvin Bashirli <ebashirli@gmail.com>
2023-12-25 00:35:19 +0400

    fix: change link

--- bot.js
+++ bot.js
@@ -4,14 +4,15 @@ const bot = new Bot(process.env.TG_BOT_TOKEN);
 bot.command("start", (ctx) => {
   console.log(ctx.update.message.from);
   console.log(ctx.update.message.chat);
-  ctx.reply("Welcome, Abdulla!", {
+  ctx.reply("Welcome", {
     reply_markup: {
       inline_keyboard: [
         [
           {
             text: "Login",
             web_app: {
-              url: `https://aspirans.om.bashiroglu.dev?chatId=${ctx.update.message.chat.id}`,
+              // TODO: add to env variable
+              url: `https://sm-om-aspirans-dev-dev-branch.vercel.app?chatId=${ctx.update.message.chat.id}`,
             },
           },
         ],
